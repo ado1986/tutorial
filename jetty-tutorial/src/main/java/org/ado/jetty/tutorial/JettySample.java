932540487417f0c8c117aa7a027465c851fcb2ef
ado1986 <duwenchao1986@sina.com>
2016-01-17 10:58:47 +0800

    增加jetty案例

---
+++ jetty-tutorial/src/main/java/org/ado/jetty/tutorial/JettySample.java
@@ -0,0 +1,10 @@
+package org.ado.jetty.tutorial;
+
+import org.eclipse.jetty.server.Server;
+
+public class JettySample {
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+		server.start();
+	}
+}
