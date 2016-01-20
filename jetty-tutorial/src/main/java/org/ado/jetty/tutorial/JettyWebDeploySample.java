e8dcf34e2db2a7666c5a3d1f2801069d972430a3
ado1986 <duwenchao1986@sina.com>
2016-01-20 10:57:54 +0800

    增加嵌入jetty的web案例

---
+++ jetty-tutorial/src/main/java/org/ado/jetty/tutorial/JettyWebDeploySample.java
@@ -0,0 +1,14 @@
+package org.ado.jetty.tutorial;
+
+/**
+ * 使用Jetty和mave-assembly结合，部署web应用
+ * 
+ * 不能用于本地调试
+ * 
+ * @author ado1986
+ *
+ * @create_time 2016年1月20日 上午9:57:17
+ */
+public class JettyWebDeploySample {
+
+}
---
+++ jetty-tutorial/src/main/java/org/ado/jetty/tutorial/JettyWebSample.java
@@ -0,0 +1,34 @@
+package org.ado.jetty.tutorial;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.xml.XmlConfiguration;
+
+/**
+ * Jetty配置web应用的例子
+ * 参考：http://wiki.eclipse.org/Jetty/Tutorial/Embedding_Jetty#Writing_Handlers
+ * 
+ * 此案例只适合本机调试，不能部署
+ * 
+ * @author ado1986
+ *
+ * @create_time 2016年1月18日 下午6:46:39
+ */
+public class JettyWebSample {
+
+	public static void main(String[] args) throws Exception {
+		Resource server_xml = Resource.newSystemResource("jetty.xml");
+		XmlConfiguration configuration = new XmlConfiguration(server_xml.getInputStream());
+		Server server = (Server) configuration.configure();
+
+		WebAppContext webapp = new WebAppContext();
+		webapp.setContextPath("/");
+		webapp.setWar("src/main/webapp");
+		server.setHandler(webapp);
+
+		server.start();
+		server.join();
+	}
+
+}
