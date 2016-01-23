4ba02a4277cfae933f0b98bb1cd63a0778e38bb8
ado1986 <duwenchao1986@sina.com>
2016-01-23 17:12:27 +0800

    增加jetty web+assembly案例

---
+++ jetty-tutorial/README.md
@@ -0,0 +1,4 @@
+# jetty-tutorial
+使用jetty的案例：
+1、JettyWebSample，使用嵌入的jetty创建了一个web应用。
+2、JettyWebDeploySample，
--- jetty-tutorial/src/main/java/org/ado/jetty/tutorial/JettyWebDeploySample.java
+++ jetty-tutorial/src/main/java/org/ado/jetty/tutorial/JettyWebDeploySample.java
@@ -1,5 +1,10 @@
 package org.ado.jetty.tutorial;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.xml.XmlConfiguration;
+
 /**
  * 使用Jetty和mave-assembly结合，部署web应用
  * 
@@ -10,5 +15,17 @@ package org.ado.jetty.tutorial;
  * @create_time 2016年1月20日 上午9:57:17
  */
 public class JettyWebDeploySample {
+	public static void main(String[] args) throws Exception {
+		Resource server_xml = Resource.newSystemResource("jetty-deploy.xml");
+		XmlConfiguration configuration = new XmlConfiguration(server_xml.getInputStream());
+		Server server = (Server) configuration.configure();
 
+		WebAppContext webapp = new WebAppContext();
+		webapp.setContextPath("/");
+		webapp.setWar("config");
+		server.setHandler(webapp);
+
+		server.start();
+		server.join();
+	}
 }
